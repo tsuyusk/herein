7e3769f8d913ee6c0bc8229c049e2ec76d9a9f92
Tsuy <rafaeldesenhos99@gmail.com>
2023-02-16 21:19:13 -0300

    Feat: Cart message when empty

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -5,7 +5,7 @@ import { useCart } from '@/hooks/cart';
 import { parsePrice } from '@/utils/parsePrice';
 import { useRouter } from 'next/router';
 import React, { useCallback } from 'react';
-import { MdArrowUpward } from 'react-icons/md';
+import { MdArrowLeft, MdArrowUpward } from 'react-icons/md';
 
 const Cart: React.FC = () => {
   const { items, addItem, removeItem, totalPrice } = useCart();
@@ -31,6 +31,16 @@ const Cart: React.FC = () => {
               <div className="flex flex-col w-full md:w-[60%]">
                 <h1 className="relative text-2xl font-bold mt-6 mb-8 w-fit after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-1 after:w-[110%] after:bg-primary">Seu carrinho</h1>
 
+                {items.length === 0 && (
+                  <div>
+                    <h2 className="text-gray-600 text-lg">Seu carrinho está vazio ):</h2>
+
+                    <button onClick={() => router.push('/')} className="bg-white mt-4 px-3 w-fit py-2 flex items-center text-primary rounded-md shadow-lg border-primary border-2 transition-all duration-500 hover:text-white hover:bg-primary">
+                      <MdArrowLeft className="mr-1" />
+                      Ver Produtos
+                    </button>
+                  </div>
+                )}
                 <div className="grid w-full auto-rows-auto grid-cols-[repeat(auto-fit,_140px)] justify-center md:justify-start gap-2 md:grid-cols-[repeat(auto-fit,240px)] md:gap-8">
                   {items.map(product => (
                     <div key={product.id} className="flex flex-col">
@@ -54,18 +64,20 @@ const Cart: React.FC = () => {
                 </div>
               </div>
 
-              <div className="flex flex-col h-fit p-4 bg-white w-full md:w-[30%] rounded-lg shadow-lg">
-                <h1 className="relative text-2xl font-bold mt-6 w-fit after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-1 after:w-[110%] after:bg-primary">Preço Total</h1>
+              {items.length !== 0 && (
+                <div className="flex flex-col h-fit p-4 bg-white w-full md:w-[30%] rounded-lg shadow-lg">
+                  <h1 className="relative text-2xl font-bold mt-6 w-fit after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-1 after:w-[110%] after:bg-primary">Preço Total</h1>
 
-                <h2 className="text-green-400 text-xl my-8">
-                  {parsePrice(totalPrice)}
-                </h2>
+                  <h2 className="text-green-400 text-xl my-8">
+                    {parsePrice(totalPrice)}
+                  </h2>
 
-                <button onClick={handleGoToCheckout} className="bg-white px-3 w-fit py-2 flex items-center text-primary rounded-md shadow-lg border-primary border-2 transition-all duration-500 hover:text-white hover:bg-primary">
-                  <MdArrowUpward className="mr-1" />
-                  Comprar
-                </button>
-              </div>
+                  <button onClick={handleGoToCheckout} className="bg-white px-3 w-fit py-2 flex items-center text-primary rounded-md shadow-lg border-primary border-2 transition-all duration-500 hover:text-white hover:bg-primary">
+                    <MdArrowUpward className="mr-1" />
+                    Comprar
+                  </button>
+                </div>
+              )}
             </div>
           </div>
         </main>
